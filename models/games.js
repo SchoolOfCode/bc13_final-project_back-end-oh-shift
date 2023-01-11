8d9f6a2b400c282ed2eee058bb9aad6d5206f4ca
soph-th <109481663+soph-th@users.noreply.github.com>
2023-01-11 17:36:18 +0000

    difficulty, number_of_players, age filters complete. genre needs changing based on multi-select options, duration needs adding

--- models/games.js
+++ models/games.js
@@ -1,71 +1,132 @@
-import { pool } from '../db/index.js'
+import { pool } from "../db/index.js";
 
-export async function createGame (newGame) {
+export async function createGame(newGame) {
   const data = await pool.query(
-    'INSERT INTO games (title, year_published, quantity, minimum_players, maximum_players, category, duration, difficulty, minimum_age, description, packaging_image_url, artwork_image_url, rules, skus, barcode, location, video_rules) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17) RETURNING *;',
-    [newGame.title, newGame.year_published, newGame.quantity, newGame.minimum_players, newGame.maximum_players, newGame.category, newGame.duration, newGame.difficulty, newGame.minimum_age, newGame.description, newGame.packaging_image_url, newGame.artwork_image_url, newGame.rules, newGame.skus, newGame.barcode, newGame.location, newGame.video_rules])
-  return data.rows[0]
+    "INSERT INTO games (title, year_published, quantity, minimum_players, maximum_players, category, duration, difficulty, minimum_age, description, packaging_image_url, artwork_image_url, rules, skus, barcode, location, video_rules) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17) RETURNING *;",
+    [
+      newGame.title,
+      newGame.year_published,
+      newGame.quantity,
+      newGame.minimum_players,
+      newGame.maximum_players,
+      newGame.category,
+      newGame.duration,
+      newGame.difficulty,
+      newGame.minimum_age,
+      newGame.description,
+      newGame.packaging_image_url,
+      newGame.artwork_image_url,
+      newGame.rules,
+      newGame.skus,
+      newGame.barcode,
+      newGame.location,
+      newGame.video_rules,
+    ]
+  );
+  return data.rows[0];
 }
 
-export async function getGamesByFilter (difficulty, number_of_players, age, duration, genre) {
+export async function getGamesByFilter(
+  difficulty,
+  number_of_players,
+  age,
+  duration,
+  genre
+) {
   // specify base of sql statement
-  const sqlParams = []
-  let sqlQuery = 'SELECT * FROM games '
+  const sqlParams = [];
+  let sqlQuery = "SELECT * FROM games ";
 
   // check to see if any filter criteria is present in url request, if so, add 'where' to sql statement
   if (difficulty || number_of_players || age || duration || genre) {
-    sqlQuery += 'WHERE '
-    console.log('sql query: ', sqlQuery)
+    sqlQuery += "WHERE ";
+    // console.log('sql query: ', sqlQuery)
   }
 
   // add to sql statement based on filter options: difficulty
   if (difficulty) {
-    sqlParams.push(difficulty)
-    sqlQuery += `difficulty = $${sqlParams.indexOf(difficulty) + 1}`
-    console.log('sql query: ', sqlQuery)
+    sqlParams.push(difficulty);
+    sqlQuery += `difficulty = $${sqlParams.indexOf(difficulty) + 1}`;
+    // console.log('sql query: ', sqlQuery)
     if (number_of_players || age || duration || genre) {
-      sqlQuery += ' AND '
+      sqlQuery += " AND ";
     }
   }
 
-    // add to sql statement based on filter options: number_of_players
+  // add to sql statement based on filter options: number_of_players
   if (number_of_players) {
-    sqlParams.push(number_of_players)
-    sqlQuery += `maximum_players >= $${sqlParams.indexOf(number_of_players) + 1} AND minimum_players <= $${sqlParams.indexOf(number_of_players) + 1}`
-    console.log('sql query: ', sqlQuery)
-    if (difficulty || age || duration || genre) {
-      sqlQuery += ' AND '
+    sqlParams.push(number_of_players);
+    sqlQuery += `maximum_players >= $${
+      sqlParams.indexOf(number_of_players) + 1
+    } AND minimum_players <= $${sqlParams.indexOf(number_of_players) + 1}`;
+    // console.log('sql query: ', sqlQuery)
+    if (age || duration || genre) {
+      sqlQuery += " AND ";
     }
   }
 
-    // add to sql statement based on filter options: age
-  if (age) {
-    sqlParams.push(age)
-    sqlQuery += `minimum_age <= $${sqlParams.indexOf(age) + 1} `
-    console.log('sql query: ', sqlQuery)
-    if (difficulty || number_of_players || duration || genre) {
-      sqlQuery += ' AND '
-    }
-  }
-
-
-    // add to sql statement based on filter options: genre
+  // add to sql statement based on filter options: genre
   if (genre) {
-    sqlParams.push(genre)
-    sqlQuery += `minimum_age <= $${sqlParams.indexOf(age) + 1} `
-    console.log('sql query: ', sqlQuery)
-    if (difficulty || number_of_players || duration || genre) {
-      sqlQuery += ' AND '
+    sqlParams.push(genre);
+    sqlQuery += `$${sqlParams.indexOf(genre) + 1} = ANY(genre) `;
+    // console.log('sql query: ', sqlQuery)
+    if (duration || age) {
+      sqlQuery += " AND ";
     }
   }
 
+//**TODO**//
+  // add to sql statement based on filter options: duration
+  //NEED TO FIND OUT DURATION TIME PERIODS TO CALCULATE RELEVANT RESULTS
+  //**TODO**//
 
-      // add to sql statement based on filter options: duration
-        //NEED TO FIND OUT DURATION TIME PERIODS TO CALCULATE RELEVANT RESULTS
+  // add to sql statement based on filter options: age
+  if (age) {
+    let ageQuery = [];
 
-  
+    for (let i = 0; i < age.length; i++) {
+      if (age[i] == 10) {
+        sqlParams.push(10);
+        ageQuery.push(`minimum_age <= $${sqlParams.indexOf(10) + 1}`);
+      }
+      if (age[i] == 12) {
+        sqlParams.push("10 AND 12");
+        ageQuery.push(
+          `minimum_age BETWEEN $${sqlParams.indexOf("10 AND 12") + 1}`
+        );
+      }
+      if (age[i] == 17) {
+        sqlParams.push("13 AND 17");
+        ageQuery.push(
+          `minimum_age BETWEEN $${sqlParams.indexOf("13 AND 17") + 1}`
+        );
+      }
+      if (age[i] == 18) {
+        sqlParams.push(18);
+        ageQuery.push(`minimum_age >= $${sqlParams.indexOf(18) + 1}`);
+      }
+      if (age[i] == 0) {
+        sqlParams.push("0 AND 100");
+        ageQuery.push(
+          `minimum_age BETWEEN $${sqlParams.indexOf("0 AND 100") + 1}`
+        );
+      }
+    }
 
-  const result = await pool.query(sqlQuery, sqlParams)
-  const games = result.rows
-  return games
+    // console.log(ageQuery)
+    let joinedAgeQuery = ageQuery.join(" OR ");
+    // console.log(joinedAgeQuery)
+
+    // sqlParams.push(age)
+    sqlQuery += `${joinedAgeQuery} `;
+    // console.log(sqlQuery)
+    // console.log('sql query: ', sqlQuery)
+  }
+
+  console.log(sqlParams);
+  console.log(sqlQuery);
+
+  const result = await pool.query(sqlQuery, sqlParams);
+  const games = result.rows;
+  return games;
 }
