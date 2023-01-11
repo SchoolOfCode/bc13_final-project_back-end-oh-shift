76a784bc2e66e4630a17b27db8200b9ba56d2239
flaviamiranda <flaviagracianom@gmail.com>
2023-01-11 09:42:17 -0300

    filter model for number of players created and working

--- models/games.js
+++ models/games.js
@@ -1,6 +1,6 @@
 import { pool } from '../db/index.js'
 
-export async function getAllGames() {
+export async function getAllGames () {
   const sqlQuery =
     'SELECT * FROM games'
   const result = await pool.query(sqlQuery)
@@ -8,31 +8,41 @@ export async function getAllGames() {
   return games
 }
 
-
 export async function createGame (newGame) {
   const data = await pool.query(
     'INSERT INTO games (title, year_published, quantity, minimum_players, maximum_players, category, duration, difficulty, minimum_age, description, packaging_image_url, artwork_image_url, rules, skus, barcode, location, video_rules) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17) RETURNING *;',
     [newGame.title, newGame.year_published, newGame.quantity, newGame.minimum_players, newGame.maximum_players, newGame.category, newGame.duration, newGame.difficulty, newGame.minimum_age, newGame.description, newGame.packaging_image_url, newGame.artwork_image_url, newGame.rules, newGame.skus, newGame.barcode, newGame.location, newGame.video_rules])
   return data.rows[0]
-  }
+}
 
 export async function getGamesByFilter (difficulty, number_of_players, age, duration, genre) {
+  // specify base of sql statement
+  const sqlParams = []
+  let sqlQuery = 'SELECT * FROM games '
 
-//specify base of sql statement
-let sqlQuery = `SELECT * FROM games `;
+  // check to see if any filter criteria is present in url request, if so, add 'where' to sql statement
+  if (difficulty || number_of_players || age || duration || genre) {
+    sqlQuery += 'WHERE '
+    console.log('sql query: ', sqlQuery)
+  }
 
-//check to see if any filter criteria is present in url request, if so, add 'where' to sql statement
-if (difficulty || number_of_players || age || duration || genre) {
-  sqlQuery += `WHERE `;
-  console.log('sql query: ', sqlQuery)
-}
+  // add to sql statement based on filter options: difficulty
+  if (difficulty) {
+    sqlParams.push(difficulty)
+    sqlQuery += `difficulty = $${sqlParams.indexOf(difficulty) + 1}`
+    console.log('sql query: ', sqlQuery)
+    if (number_of_players || age || duration || genre) {
+      sqlQuery += ' AND '
+    }
+  }
 
-//add to sql statement based on filter options: difficulty
-if (difficulty) {
-  sqlQuery += `difficulty = $1`
-  console.log('sql query: ', sqlQuery)
-}
-const result = await pool.query(sqlQuery, [difficulty])
-const games = result.rows
-return games
+  if (number_of_players) {
+    sqlParams.push(number_of_players)
+    sqlQuery += `maximum_players >= $${sqlParams.indexOf(number_of_players) + 1} AND minimum_players <= $${sqlParams.indexOf(number_of_players) + 1}`
+    console.log('sql query: ', sqlQuery)
+  }
+
+  const result = await pool.query(sqlQuery, sqlParams)
+  const games = result.rows
+  return games
 }
--- routes/games.js
+++ routes/games.js
@@ -12,7 +12,6 @@ gamesRouter.get('/', async function (req, res) {
   })
 })
 
-
 gamesRouter.post('/', async function (req, res) {
   console.log(req.body)
   const newGame = req.body
@@ -20,17 +19,17 @@ gamesRouter.post('/', async function (req, res) {
   res.status(201).json({ success: true, payload: result })
 })
 
-//ultimate filter that adds filter conditions (difficulty, number_of_players, age, duration, genre)
-gamesRouter.get("/filter", async function (req, res) {
-  const difficulty = req.query.difficulty;
+// ultimate filter that adds filter conditions (difficulty, number_of_players, age, duration, genre)
+gamesRouter.get('/filter', async function (req, res) {
+  const difficulty = req.query.difficulty
   console.log('console log difficulty', difficulty)
-  const number_of_players = req.query.number_of_players;
-  const age = req.query.age;
-  const duration = req.query.duration;
-  const genre = req.query.genre;
-  const games = await gamesModel.getGamesByFilter(difficulty, number_of_players, age, duration, genre);
+  const number_of_players = req.query.number_of_players
+  const age = req.query.age
+  const duration = req.query.duration
+  const genre = req.query.genre
+  const games = await gamesModel.getGamesByFilter(difficulty, number_of_players, age, duration, genre)
   res.status(200).json({
     success: true,
     payload: games
   })
-});
+})
