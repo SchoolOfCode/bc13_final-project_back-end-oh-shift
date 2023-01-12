1728ad372212fbc608be04a54b67e4ebeb4ab3d3
soph-th <109481663+soph-th@users.noreply.github.com>
2023-01-12 11:20:01 +0000

    AGE QUERY IS WORKING, WOOOOO

--- models/games.js
+++ models/games.js
@@ -75,7 +75,6 @@ export async function getGamesByFilter(
     }
   }
 
-//**TODO**//
   // add to sql statement based on filter options: duration
   if (duration) {
     console.log('DURATION IF STATEMENT REACHED')
@@ -84,32 +83,31 @@ export async function getGamesByFilter(
     userDuration.push(duration)
 
     for (let i = 0; i <= userDuration.length; i++) {
-      console.log('HELLO LOOP')
-      console.log(duration.length)
-
-      if (userDuration[i] == 30) {
+      if (duration[i] == '30' || userDuration[i] == '30') {
+        console.log('USER DURATION[I] == 30')
         sqlParams.push(30);
         durationQuery.push(`duration <= $${sqlParams.indexOf(30) + 1}`);
       }
-      if (userDuration[i] == 60) {
-        sqlParams.push(30, 90);
-        durationQuery.push(
-          `duration BETWEEN $${sqlParams.indexOf(30) + 1} AND $${sqlParams.indexOf(90) + 1}`
-        );
+      if (duration[i] == '60' || userDuration[i] == '60') {
+        console.log('USER DURATION[I] == 60')
+        sqlParams.push(29, 89);
+        durationQuery.push(`duration BETWEEN $${sqlParams.indexOf(29) + 1} AND $${sqlParams.indexOf(89) + 1}`);
       }
-      if (userDuration[i] == 90) {
+      if (duration[i] == '90' || userDuration[i] == '90') {
+        console.log('USER DURATION[I] == 90')
         sqlParams.push(90);
         durationQuery.push(`duration >= $${sqlParams.indexOf(90) + 1}`);
       }
-      // if (duration[i] == 'any') {
-      //   sqlParams.push(0);
-      //   durationQuery.push(
-      //     `duration > $${sqlParams.indexOf(0) + 1}`
-      //   );
-      // }
+
+      if (duration[i] == 'any'  || userDuration[i] == 'any') {
+        sqlParams.push(0);
+        durationQuery.push(
+          `duration > $${sqlParams.indexOf(0) + 1}`
+        );
+      }
     }
 
-    console.log(durationQuery)
+    console.log('duration query', durationQuery)
     let joinedDurationQuery = durationQuery.join(" OR ")
     if ( difficulty || number_of_players || age || genre ) {
       joinedDurationQuery= joinedDurationQuery+')';
@@ -124,30 +122,33 @@ export async function getGamesByFilter(
 
   // add to sql statement based on filter options: age
   if (age) {
+    console.log('AGE = ', age)
     let ageQuery = [];
+    let userAge = [];
+    userAge.push(age)
 
-    for (let i = 0; i < age.length; i++) {
-      if (age[i] == 10) {
+    for (let i = 0; i < userAge.length; i++) {
+      if (userAge[i] == 10) {
         sqlParams.push(10);
         ageQuery.push(`minimum_age <= $${sqlParams.indexOf(10) + 1}`);
       }
-      if (age[i] == 12) {
+      if (userAge[i] == 12) {
         sqlParams.push(10, 12);
         ageQuery.push(
           `minimum_age BETWEEN $${sqlParams.indexOf(10) + 1} AND $${sqlParams.indexOf(12) + 1}`
         );
       }
-      if (age[i] == 17) {
+      if (userAge[i] == 17) {
         sqlParams.push(13, 17);
         ageQuery.push(
           `minimum_age BETWEEN $${sqlParams.indexOf(13) + 1} AND $${sqlParams.indexOf(17) + 1}`
         );
       }
-      if (age[i] == 18) {
+      if (userAge[i] == 18) {
         sqlParams.push(18);
         ageQuery.push(`minimum_age >= $${sqlParams.indexOf(18) + 1}`);
       }
-      if (age[i] == 0) {
+      if (userAge[i] == 0) {
         sqlParams.push(0, 100);
         ageQuery.push(
           `minimum_age BETWEEN $${sqlParams.indexOf(0) + 1} AND $${sqlParams.indexOf(100) + 1}`
@@ -157,8 +158,8 @@ export async function getGamesByFilter(
 
     // console.log(ageQuery)
     let joinedAgeQuery = ageQuery.join(" OR ")
-    if ( difficulty || number_of_players || duration || genre) {
 
+    if ( difficulty || number_of_players || duration || genre) {
       joinedAgeQuery= joinedAgeQuery+')';
     }
   
@@ -166,12 +167,15 @@ export async function getGamesByFilter(
 
 
     sqlQuery += `${joinedAgeQuery} `;
+    console.log(joinedAgeQuery)
+
     // console.log(sqlQuery)
     // console.log('sql query: ', sqlQuery)
   }
 
   console.log(sqlParams);
   console.log(sqlQuery);
+  
 
   const result = await pool.query(sqlQuery, sqlParams);
   const games = result.rows;
