f36954311f6acd00a354b08d9025d3b8d5f1e626
[Creg Grady] <112422397+CregCodes@users.noreply.github.com>
2023-01-16 17:20:36 +0000

    test on combined filter to finish- notes in js docs

--- models/games.test.js
+++
@@ -1,8 +0,0 @@
-import { getByFilter } from "./games";
-
-describe('', () => {
-    test('test handles empty args', () => {
-        const [query, params] = getByFilter(null, null, null, null, null)
-        expect(query).toBe('SELECT * FROM games;')
-        expect(params).toEqual([])
-    });})
