d9aea7a22d8c2526dc6f6f3fb5b5fe52d8cac01e
Soph T <109481663+soph-th@users.noreply.github.com>
2023-01-21 22:50:50 +0000

    Update reviews.js

--- models/reviews.js
+++ models/reviews.js
@@ -2,13 +2,14 @@ import { pool } from "../db/index.js";
 
 export async function createReview(newReview) {
   const data = await pool.query(
-    "INSERT INTO reviews (game_id, review_text, rating, user_given_name, user_picture) VALUES ($1,$2,$3,$4,$5) RETURNING *;",
+    "INSERT INTO reviews (game_id, review_text, rating, user_given_name, user_picture, user_id) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *;",
     [
       newReview.game_id,
       newReview.review_text,
       newReview.rating,
       newReview.user_given_name,
       newReview.user_picture,
+      newReview.user_id
     ]
   );
   return data.rows[0];
@@ -16,7 +17,7 @@ export async function createReview(newReview) {
 
 export async function getByGameId(gameId) {
   const data = await pool.query(
-    "SELECT review_id, reviews.date_added, game_id, review_text,rating, user_given_name, user_picture, title FROM reviews JOIN games ON reviews.game_id= games.id WHERE games.id=$1 ORDER BY reviews.date_added DESC;",
+    "SELECT review_id, reviews.date_added, game_id, review_text,rating, user_given_name, user_picture, user_id, title FROM reviews JOIN games ON reviews.game_id= games.id WHERE games.id=$1 ORDER BY reviews.date_added DESC;",
     [gameId]
   );
   return data.rows;
