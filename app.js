b283cf3054ae285d3f3a218dbd1d3276ef2f3f91
[Creg Grady] <112422397+CregCodes@users.noreply.github.com>
2023-01-21 15:16:01 +0000

    review routes and models set up

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-import express from 'express'
-import morgan from 'morgan'
-import cors from 'cors'
-import { gamesRouter } from './routes/games.js'
-const app = express()
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { gamesRouter } from "./routes/games.js";
+import { reviewsRouter } from "./routes/reviews.js";
+const app = express();
 
-app.use(cors('*'))
-app.use(morgan('dev'))
-app.use(express.json())
-app.use('/api/games', gamesRouter)
-// app.use('/api/posts', postsRouter)
-// app.use('/api/comments', commentsRouter)
+app.use(cors("*"));
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/api/games", gamesRouter);
+app.use("/api/reviews", reviewsRouter);
 
 app.use(function (req, res, next) {
   res.status(404).json({
     success: false,
     error:
-        "We couldn't find what you were looking for 😞. Did you send the request to the right path?"
-  })
-})
+      "We couldn't find what you were looking for 😞. Did you send the request to the right path?",
+  });
+});
 
 app.use(function (err, req, res, next) {
-  console.error(err.stack)
+  console.error(err.stack);
   res.status(500).json({
     success: false,
-    error: 'Something went wrong, please try again later'
-  })
-})
+    error: "Something went wrong, please try again later",
+  });
+});
 
 export default app;
