e8e6f0ea54c451deb68ffe06e018685e84e76a88
soph-th <109481663+soph-th@users.noreply.github.com>
2023-01-24 14:28:37 +0000

    games routes and models documentation added

--- routes/games.js
+++ routes/games.js
@@ -3,7 +3,13 @@ import * as gamesModel from "../models/games.js";
 
 export const gamesRouter = express.Router();
 
-/** Dev Ex post route for adding manually to new database */
+// 🤖 POST createGame
+/**
+ * Dev Ex POST route for adding new games to database.
+ * Accessed at '.../api/games'
+ * Invokes createGame function, providing argument of 'newGame' object created by request body
+ * Returns status code, success boolean and payload(confirmation of newGame)
+ */
 gamesRouter.post("/", async function (req, res) {
   console.log(req.body);
   const newGame = req.body;
@@ -11,7 +17,13 @@ gamesRouter.post("/", async function (req, res) {
   res.status(201).json({ success: true, payload: result });
 });
 
-/** ultimate filter that adds filter conditions (difficulty, number_of_players, age, duration, genre)*/
+// 🤖 GET getByFilter
+/**
+ * GET route for returning games from database depending on some, all or no filter values.
+ * Accessed at '.../api/games?<<parameters go here>>' e.g. '.../api/games?difficulty=hard&genre=adventure'
+ * Invokes getByFilter function using any filter parameters provided in url as argument
+ * Returns status code, success boolean and payload(array of games that match filter parameters)
+ */
 gamesRouter.get("/", async function (req, res) {
   const difficulty = req.query.difficulty;
   const number_of_players = req.query.number_of_players;
@@ -38,7 +50,13 @@ gamesRouter.get("/", async function (req, res) {
   });
 });
 
-/** Route- get by ID- ability to search/access a full description of a game */
+// 🤖 GET getById
+/**
+ * GET route for returning games from database where game id = url parameter id
+ * Accessed at '.../api/games/:id'
+ * Invokes getById function using url id as argument
+ * Returns status code, success boolean and payload(game object)
+ */
 gamesRouter.get("/:id", async function (req, res) {
   const id = req.params.id;
   const game = await gamesModel.getByID(id);
@@ -48,21 +66,13 @@ gamesRouter.get("/:id", async function (req, res) {
   });
 });
 
-/**Route- ability to search/ access a data response for GENRE options
- * Options = same as result of fetch via this route
- */
-gamesRouter.get("/genre", async function (req, res) {
-  const category = req.params.category;
-  console.log(category);
-  const options = await gamesModel.filterHandler(category);
-  res.status(200).json({
-    success: true,
-    payload: options,
-  });
-});
 
-/**Route- ability to search/ access a data response with filter options applied
- * Options = same as result of fetch via this route
+// 🤖 GET <category>FilterHandler
+/**
+ * GET route for returning list of available options to search databse by for each filter category, used to populate options for front-end dropdown filter components
+ * Accessed at '.../api/games/filters/:category'
+ * Invokes either genreFilterHandler, difficultyFilterHandler or durationFilterHandler function depending on url category
+ * Returns status code, success boolean and payload(array of options)
  */
 gamesRouter.get("/filters/:category", async function (req, res) {
   const category = req.params.category;
